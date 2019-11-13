2eaffd39257af3ae80a581015fdb53e92005c4af
fratim <frtim@student.ethz.ch>
2019-11-13 10:05:51 -0500

    updated meta file

--- connectome/wiring.pyx
+++ connectome/wiring.pyx
@@ -17,7 +17,6 @@ cdef extern from 'cpp-wiring.h':
     void CppUpdateResolution(float resolution[3])
     void CppUpdateGridsize(long gridsize[3])
     void CppSkeletonGeneration(const char *prefix, long label, const char *lookup_table_directory, long *labels)
-    void CppSkeletonRefinement(const char *prefix, long label, double resolution[3])
 
 # extract the wiring diagram for this prefix and label
 def GenerateSkeleton(prefix, label, block_z, block_y, block_x):
@@ -34,7 +33,7 @@ def GenerateSkeleton(prefix, label, block_z, block_y, block_x):
     CppUpdateResolution(&(cpp_resolution[0]))
 
     # get, check and set grid size from meta file
-    gridsize = dataIO.GridSize(prefix)
+    gridsize = dataIO.BlockSize(prefix)
     if data.shape[0]!=gridsize[0] or data.shape[1]!=gridsize[1] or data.shape[2]!=gridsize[2]:
         raise ValueError("Data chunk size not equal to size specified in meta file!")
     #cdef np.ndarray[float, ndim=1, mode='c'] cpp_gridsize = np.ascontiguousarray(gridsize).astype(np.float32)
@@ -51,19 +50,3 @@ def GenerateSkeleton(prefix, label, block_z, block_y, block_x):
 
     # print out statistics for wiring extraction
     print ('Generated skeletons in {:0.2f} seconds'.format(time.time() - start_time))
-
-
-
-# post process the volume to correct segment errors
-def RefineSkeleton(prefix, label):
-    # start running time statistics
-    start_time = time.time()
-
-    # get the resolution for this data
-    cdef np.ndarray[double, ndim=1, mode='c'] cpp_resolution = np.ascontiguousarray(dataIO.Resolution(prefix))
-
-    # call the post processing algorithm
-    CppSkeletonRefinement(prefix.encode('utf-8'), label, &(cpp_resolution[0]))
-
-    # print out statistics
-    print ('Refined skeletons in {:0.2f} seconds'.format(time.time() - start_time))
--- data_structures/meta_data.py
+++ data_structures/meta_data.py
@@ -20,14 +20,34 @@ class MetaData:
                     samples = value.split('x')
                     # need to use 2, 1, and 0 here since the outward facing convention is x,y,z, not z, y, x
                     self.resolution = (float(samples[2]), float(samples[1]), float(samples[0]))
-                elif comment == '# grid size':
+                elif comment == '# volume size':
                     # separate into individual dimensions
                     samples = value.split('x')
                     # need to use 2, 1, and 0 here since the outward facing convention is x,y,z, not z, y, x
-                    self.grid_size = (int(samples[2]), int(samples[1]), int(samples[0]))
+                    self.volume_size = (int(samples[2]), int(samples[1]), int(samples[0]))
+                elif comment == '# block size':
+                    # separate into individual dimensions
+                    samples = value.split('x')
+                    # need to use 2, 1, and 0 here since the outward facing convention is x,y,z, not z, y, x
+                    self.block_size = (int(samples[2]), int(samples[1]), int(samples[0]))
+                elif comment == '# blocks filepath':
+                    self.blocks_filepath = str(value)
+                elif comment == '# synapses filepath':
+                    self.synapses_filepath = str(value)
+                else:
+                    raise ValueError("Unknown keyword in metafile")
 
     def Resolution(self):
         return self.resolution
 
     def GridSize(self):
-        return self.grid_size
+        return self.volume_size
+
+    def BlockSize(self):
+        return self.block_size
+
+    def BlocksFilepath(self):
+        return self.blocks_filepath
+
+    def SynapsesFilepath(self):
+        return self.synapses_filepath
