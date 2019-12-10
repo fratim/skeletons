8319ede2b0537fa52e193b5dc8a054bb4e63d1c6
fratim <frtim@student.ethz.ch>
2019-12-10 09:33:16 -0500

    updated step01 folder init file

--- examples/scripts/step1.py
+++ examples/scripts/step1.py
@@ -11,6 +11,7 @@ output_directories = [  'synapses_projected/',
 'walls/',
 'segment_IDs/',
 'skeleton/',
+'somae_surface',
 'distances/',
 'widths/',
 'running_times/',
@@ -26,6 +27,10 @@ if not os.path.exists(output_folder): os.mkdir(output_folder)
 output_folder_skeletons = dataIO.OutputDirectory(prefix)+'skeletons/'
 if not os.path.exists(output_folder_skeletons): os.mkdir(output_folder_skeletons)
 if not os.path.exists('{}/{}'.format(output_folder_skeletons, prefix)): os.mkdir('{}/{}'.format(output_folder_skeletons, prefix))
+
+output_folder_times = dataIO.OutputDirectory(prefix)+'running_times/'
+if not os.path.exists(output_folder_times): os.mkdir(output_folder_times)
+if not os.path.exists('{}/{}'.format(output_folder_times, prefix)): os.mkdir('{}/{}'.format(output_folder_times, prefix))
 
 for bz in range(start_blocks[0], start_blocks[0]+n_blocks[0]):
     for by in range(start_blocks[2], start_blocks[1]+n_blocks[1]):
